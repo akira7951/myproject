1bc064abcf1abed5b5750c36512fdafc0c4b6cc0
Lawrence Hu <69295147+akira7951@users.noreply.github.com>
2024-06-25 15:47:49 +0800

    Delete aegis directory

--- aegis/layout/footer.php
+++
@@ -1,5 +0,0 @@
-    <script src="js/jquery-3.5.1.min.js"></script>
-    <script src="bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
-    <script src="js/popper.min.js"></script>
-</body>
-</html>
